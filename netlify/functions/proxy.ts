4b88fda721733c645ca1e5f18daa227c424e539b
SpiralTower <spiraltower@outlook.com>
2024-11-20 20:15:10 +0800

    update

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -28,7 +28,34 @@ export default async (request: Request, context: Context) => {
   }
 
   const { pathname, searchParams } = new URL(request.url);
-
+  if(pathname === "/") {
+    let blank_html = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>xAI API proxy on Netlify Edge</title>
+</head>
+<body>
+  <h1 id="x-api-proxy-on-netlify-edge">xAI API proxy on Netlify Edge</h1>
+  <p>This project is forked from <a href="https://github.com/antergone/palm-netlify-proxy" target="_blank">palm-netlify-proxy</a></p>
+  <p>Tips: This project uses a reverse proxy to solve problems such as location restrictions in xAI APIs. </p>
+  <p>If you have any of the following requirements, you may need the support of this project.</p>
+  <ol>
+  <li>When you see the error message &quot;User location is not supported for the API use&quot; when calling the xAI API</li>
+  <li>You want to customize the xAI PaLM API</li>
+  </ol>
+  <p>For technical discussions, please visit <a href="https://simonmy.com/posts/使用netlify反向代理google-palm-api.html">https://simonmy.com/posts/使用netlify反向代理google-palm-api.html</a></p>
+</body>
+</html>
+    `
+    return new Response(blank_html, {
+      headers: {
+        ...CORS_HEADERS,
+        "content-type": "text/html"
+      },
+    });
+  }
 
   const url = new URL(pathname, "https://api.x.ai");
   searchParams.delete("_path");
